576bd30ab038326535a8aed5a23aa5ab110ff189
Michelle Cali <mctexx@gmail.com>
2016-06-15 17:04:31 +0000

    Submitting code

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,16 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
+  list = []
   
+  if arr.length > 1
+    arr.each_index do |i|
+      if (arr[i] != arr[i+1] && arr[i] != arr[i-1])
+        list << arr[i]
+      end
+    end
+  else 
+    list = arr
+  end
+  return list
 end
